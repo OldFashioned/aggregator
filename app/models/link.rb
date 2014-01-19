dca4628bda44e4cea7b01f7fb90d582cb5105dbc
Robert McCaffery <robert.j.mccaffery@gmail.com>
2014-01-19 10:14:21 -0500

    added link sanitizing

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,9 @@
 class Link < ActiveRecord::Base
+	before_save :sanitize_url
 
+	def sanitize_url
+    	unless self.url.include?("http://") || self.url.include?("https://")
+      		self.url = "http://" + self.url
+    	end
+  	end
 end
